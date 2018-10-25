a40f0216e79feb2c800006e7c2f5399a59dc96be
mm <mm2tmm@gmail.com>
2018-10-25 17:01:02 +0330

    try start seperate connection

--- site.js
+++ site.js
@@ -1,7 +1,9 @@
 var mysql = require('mysql');
 //var connection = require("./connection");
 
-//var mysql = require('mysql');
+var mysql = require('mysql');
+
+//try seperate connection
 
 var connection = mysql.createConnection({
     host: "localhost",
