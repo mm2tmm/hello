52a52833e00b21f5778f24e8fcae7381df3ae1e2
mm <mm2tmm@gmail.com>
2018-10-25 12:51:18 +0330

    Initial commit

---
+++ site.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+var connection = require("./connection");
+
+
+function getAllSites(callback)
+{
+    connection.query('SELECT * FROM mm_mcontent4_sites where published=1',
+        function(err, result)
+        {
+            if (err)
+                callback(err,null);
+            else
+                callback(null,result[0]);
+
+        }
+    );
+}
+
+module.exports.getAllSites = getAllSites;
+
