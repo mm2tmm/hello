2eccaf50462b6335481479b33bd0e47d79e37a3f
mm <mm2tmm@gmail.com>
2018-10-25 18:07:29 +0330

    ok - get all pages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var db = require("./db");
 
-db.getRows('SELECT id,title FROM mm_mcontent4_sites where published=1',function(err,sites){
+//get all sites
+db.getRows('SELECT * FROM mm_mcontent4_sites where published=1',function(err,sites){
     if (err) {
         console.log("ERROR : ",err);
     } else {
@@ -9,6 +10,22 @@ db.getRows('SELECT id,title FROM mm_mcontent4_sites where published=1',function(
             console.log("start site : " + site.title);
 
 
+            //get all pages
+            db.getRows('SELECT * FROM mm_mcontent4_pages where site='+site.id ,function(err,pages){
+                if (err) {
+                    console.log("ERROR : ",err);
+                } else {
+                    for (var i = 0; i < pages.length; i++) {
+                        var page = pages[i];
+                        console.log("start page : " + page.title);
+
+                        
+                        //end of each page.
+                    }
+                }
+            });
+
+            //end of for each site
         }
     }
 });
