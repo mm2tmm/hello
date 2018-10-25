cf73c328573b2571b804dea9f27e8615ef92812f
mm <mm2tmm@gmail.com>
2018-10-25 18:30:11 +0330

    ok - add simple scrape

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
 var db = require("./db");
+const puppeteer = require('puppeteer');
+
+let scrape = async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+
+    await page.goto('http://books.toscrape.com/');
+    await page.click('#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(1) > article > div.image_container > a > img');
+    await page.waitFor(1000);
+
+    const result = await page.evaluate(() => {
+        let title = document.querySelector('h1').innerText;
+        let price = document.querySelector('.price_color').innerText;
+
+        return {
+            title,
+            price
+        }
+
+    });
+
+    browser.close();
+    return result;
+};
+
+scrape().then((value) => {
+    console.log(value); // Success!
+});
 
 //get all sites
 db.getRows('SELECT * FROM mm_mcontent4_sites where published=1',function(err,sites){
@@ -19,7 +47,7 @@ db.getRows('SELECT * FROM mm_mcontent4_sites where published=1',function(err,sit
                         var page = pages[i];
                         console.log("start page : " + page.title);
 
-                        
+
                         //end of each page.
                     }
                 }
